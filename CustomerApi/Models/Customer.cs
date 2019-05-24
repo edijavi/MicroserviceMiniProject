88a9907ff7233be2ea6a984b51c8f7321bb9ae66
edijavi <edilata@msn.com>
2019-05-25 01:25:32 +0200

    .

---
+++ CustomerApi/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharedModels
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+        public decimal CreditStanding { get; set; }
+        
+    }
+}
